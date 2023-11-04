aacdcea5f830e94b63b38f1121ec86a46f203327
Tapash <tapasp263@gmail.com>
2023-11-04 17:50:29 +0600

    update again

--- src/components/Carddetails.js
+++ src/components/Carddetails.js
@@ -2,11 +2,11 @@ import { Component } from "react";
 
 class Carddetails extends Component{
     render(){
-        const { match } = this.props;
-        const { postId } = match.params;
+        // const { match } = this.props;
+        // const { postId } = match.params;
         return (
             <div>
-                new id  {postId}
+                new id  
             </div>
         )
     }
